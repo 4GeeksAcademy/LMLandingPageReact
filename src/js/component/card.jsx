e905e434c52b695fe135dfecfd792d4954126eb3
Letimachado <leti.machado.gerez@gmail.com>
2024-07-05 12:05:50 +0000

    Casi

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card() {
     return (
         <div className="card col-md-2 col-sm-12">
-            <img src="..." className="card-img-top" alt="..." />
+            <img src="https://picsum.photos/seed/picsum/200/300" className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,21 +2,28 @@ import React from "react";
 import Navbar from "./navbar";
 import Jumbotron from "./jumbotron";
 import Card from "./card";
+import Footer from "./footer"
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
+		<>
 			<Navbar />
 			<div className="container">
 				<Jumbotron />
-				<Card />
+					<div className="gap-4 d-flex flex-sm-row flex-column row justify-content-center">
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+					</div>
+					<Footer />				
 			</div>
 			
 			
-		</div>
+		</>
 	);
 };
 
