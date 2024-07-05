96a84445e170f4683b94489913af67dab02f2590
Letimachado <leti.machado.gerez@gmail.com>
2024-07-05 12:11:54 +0000

    Casi listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./navbar";
 import Jumbotron from "./jumbotron";
 import Card from "./card";
-import Footer from "./footer"
+import Footer from "./footer";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
